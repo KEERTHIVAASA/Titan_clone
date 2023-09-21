5c59abd2118a4fb6e1b2d475b575bb8c8bdee5b8
keerthivaasan <keerthivaasansk@gmail.com>
2023-09-21 17:32:07 +0530

    Email Trigger enabled

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,79 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegistrationForm
+from .models import Accounts
+from django.contrib import  messages,auth
+from django.contrib.auth.decorators import login_required
+
+#verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage 
+
 
 def register(request):
-    return render(request,'accounts/register.html')
+    if request.method=='POST':
+        form= RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name=form.cleaned_data['first_name']
+            last_name=form.cleaned_data['last_name']
+            email=form.cleaned_data['email']
+            phone_number=form.cleaned_data['phone_number']
+            password=form.cleaned_data['password']
+            username=email.split("@")[0]
+            
+            user = Accounts.object.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)
+            user.phone_number=phone_number
+            user.save()
+            
+            #USER ACTIVATION
+            
+            current_site=get_current_site(request)
+            mail_subject='Please activate your account'
+            message=render_to_string('accounts/activation.html',{
+                'user':user,
+                'domain':current_site,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user)
+                
+            })
+            to_email=email
+            send_email= EmailMessage(mail_subject,message,to=[to_email])
+            send_email.send()
+            messages.success(request,'Registration successfull.')
+            return redirect('register')
+    else:
+        form=RegistrationForm()
+    context={
+        'form': form,
+    }
+    return render(request,'accounts/register.html',context)
 
 def login(request):
+    if request.method=='POST':
+        email=request.POST['email']
+        password=request.POST['password']
+        
+        user=auth.authenticate(email=email,password=password)
+        
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'Invalid Credentials')
+            return redirect('login')
     return render(request,'accounts/login.html')
 
+
+
+@login_required(login_url='login')
 def logout(request):
+    
+    auth.logout(request)
+    messages.success(request,"You have Logged out")
+    return redirect('login')
+
+def activate(request):
     return 
